803c15663886e627c4a467596d1c9fb4bbd71370
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-11-03 08:05:21 +0200

    add print and let statements

---
+++ lib/statements.dart
@@ -0,0 +1,28 @@
+import "expressions.dart";
+
+sealed class Statement<T> {
+  T execute(Map<String, num> variables);
+}
+
+class LetStatement extends Statement<void> {
+  final String identifier;
+  final Expression expression;
+
+  LetStatement(this.identifier, this.expression);
+
+  @override
+  void execute(Map<String, num> variables) {
+    variables[identifier] = expression.evaluate(variables);
+  }
+}
+
+class PrintStamement extends Statement<String> {
+  final List<Expression> arguments;
+
+  PrintStamement(this.arguments);
+
+  @override
+  String execute(Map<String, num> variables) {
+    return arguments.map((expr) => expr.evaluate(variables)).join("\t");
+  }
+}
