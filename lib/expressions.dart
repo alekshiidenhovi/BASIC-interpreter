4823a8168a8a34c31686699bc2e3d53624f69c71
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-11-12 16:34:22 +0200

    update comparison documentation

--- lib/expressions.dart
+++ lib/expressions.dart
@@ -1,12 +1,24 @@
-import "operators.dart";
+import 'operators.dart';
 
+/// Represents an abstract expression that can be evaluated given a set of variables.
+///
+/// The type parameter `T` represents the return type of the expression when evaluated.
 sealed class Expression<T> {
+  /// Evaluates the expression using the provided [variables].
+  ///
+  /// The [variables] map should contain mappings from variable names (Strings) to their
+  /// numerical values (num).
   T evaluate(Map<String, num> variables);
 }
 
+/// Represents a literal number within an expression.
+///
+/// This expression always evaluates to its constant [value].
 class NumberLiteralExpression extends Expression<num> {
+  /// The numerical value of this literal.
   final num value;
 
+  /// Creates a new [NumberLiteralExpression] with the given [value].
   NumberLiteralExpression(this.value);
 
   @override
@@ -15,9 +27,14 @@ class NumberLiteralExpression extends Expression<num> {
   }
 }
 
+/// Represents a literal string within an expression.
+///
+/// This expression always evaluates to its constant [value].
 class StringLiteralExpression extends Expression<String> {
+  /// The string value of this literal.
   final String value;
 
+  /// Creates a new [StringLiteralExpression] with the given [value].
   StringLiteralExpression(this.value);
 
   @override
@@ -26,22 +43,40 @@ class StringLiteralExpression extends Expression<String> {
   }
 }
 
+/// Represents a variable identifier within an expression.
+///
+/// This expression evaluates to the numerical value associated with the [identifier]
+/// in the provided variables map.
 class IdentifierExpression extends Expression<num> {
+  /// The name of the identifier.
   final String identifier;
 
+  /// Creates a new [IdentifierExpression] with the given [identifier].
   IdentifierExpression(this.identifier);
 
   @override
   num evaluate(Map<String, num> variables) {
+    // Assumes the identifier will always be present in the variables map.
+    // Consider adding error handling for cases where the identifier might be missing.
     return variables[identifier]!;
   }
 }
 
+/// Represents a comparison operation between two numerical expressions.
+///
+/// This expression evaluates to a boolean indicating whether the [lhs] and [rhs]
+/// satisfy the given [operator].
 class ComparisonExpression extends Expression<bool> {
+  /// The left-hand side of the comparison.
   final Expression<num> lhs;
+
+  /// The right-hand side of the comparison.
   final Expression<num> rhs;
+
+  /// The comparison operator to apply to the [lhs] and [rhs].
   final ComparisonOperator operator;
 
+  /// Creates a new [ComparisonExpression] with the given [lhs], [rhs], and [operator].
   ComparisonExpression(this.lhs, this.rhs, this.operator);
 
   @override
@@ -49,6 +84,7 @@ class ComparisonExpression extends Expression<bool> {
     num leftValue = lhs.evaluate(variables);
     num rightValue = rhs.evaluate(variables);
 
+    // TODO: Implement other comparison operators (e.g., greater than, less than).
     return switch (operator) {
       ComparisonOperator.equals => leftValue == rightValue,
     };
