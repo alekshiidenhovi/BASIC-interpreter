585eebbe7912314706e1dba8fe51216a3d643015
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-11-14 11:39:01 +0200

    group literal test together

--- test/lexer_test.dart
+++ test/lexer_test.dart
@@ -3,26 +3,30 @@ import 'package:basic_interpreter/tokens.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Number literals: 10 4.2', () {
-    var lexer = Lexer('10 4.2');
-    expect(lexer.tokenize(), [
-      Token(Category.numberLiteral, '10'),
-      Token(Category.numberLiteral, '4.2'),
-    ]);
-  });
+  group("Literals", () {
+    test('Number literals: 10 4.2', () {
+      var lexer = Lexer('10 4.2');
+      expect(lexer.tokenize(), [
+        Token(Category.numberLiteral, '10'),
+        Token(Category.numberLiteral, '4.2'),
+      ]);
+    });
 
-  test('String literals: "Hello, BASIC!"', () {
-    var lexer = Lexer('"Hello, BASIC!"');
-    expect(lexer.tokenize(), [Token(Category.stringLiteral, 'Hello, BASIC!')]);
-  });
+    test('String literals: "Hello, BASIC!"', () {
+      var lexer = Lexer('"Hello, BASIC!"');
+      expect(lexer.tokenize(), [
+        Token(Category.stringLiteral, 'Hello, BASIC!'),
+      ]);
+    });
 
-  test('String literals: "Hello", "BASIC"', () {
-    var lexer = Lexer('"Hello", "BASIC"');
-    expect(lexer.tokenize(), [
-      Token(Category.stringLiteral, 'Hello'),
-      Token(Category.comma, ','),
-      Token(Category.stringLiteral, 'BASIC'),
-    ]);
+    test('String literals: "Hello", "BASIC"', () {
+      var lexer = Lexer('"Hello", "BASIC"');
+      expect(lexer.tokenize(), [
+        Token(Category.stringLiteral, 'Hello'),
+        Token(Category.comma, ','),
+        Token(Category.stringLiteral, 'BASIC'),
+      ]);
+    });
   });
 
   group("Keywords and identifiers", () {
