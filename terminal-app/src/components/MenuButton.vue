2f26d2721c6dc79eb090616a42a5d07c26f64c1b
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2026-01-31 16:14:23 +0200

    add MenuButton component

---
+++ terminal-app/src/components/MenuButton.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface Props {
+  buttonText: string;
+  href: string;
+}
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <a :href="props.href">
+    {{ props.buttonText }}
+  </a>
+</template>
+
+<style>
+a {
+  text-decoration: none;
+  border: 2px solid var(--sky-500);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.875rem 1.5rem;
+  border-radius: 4px;
+  color: var(--sky-600);
+  font-size: 1.5rem;
+  letter-spacing: 1px;
+  width: 100%;
+  text-transform: uppercase;
+}
+</style>
