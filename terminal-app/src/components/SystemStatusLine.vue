e83ae91bf556e5dde7bb34a221095db502e6979e
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2026-02-02 09:07:07 +0200

    add online status to system info

--- terminal-app/src/components/SystemStatusLine.vue
+++ terminal-app/src/components/SystemStatusLine.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import { Icon } from "@iconify/vue";
+import { assertNever } from "@/utils";
 
 interface SystemBatteryStatus {
   level: number;
   isCharging: boolean;
 }
 
+type SystemNetworkStatus = {
+  onlineStatus: "online" | "offline";
+}
+
+const hasMounted = ref(false);
 const systemBatteryStatus = ref<SystemBatteryStatus | null>(null);
+const systemNetworkStatus = ref<SystemNetworkStatus | null>(null);
 
 const updateBatteryStatus = (battery: BatteryManager) => {
   systemBatteryStatus.value = {
@@ -15,6 +22,19 @@ const updateBatteryStatus = (battery: BatteryManager) => {
     isCharging: battery.charging,
   }
 }
+
+const turnOnlineStatus = () => {
+  systemNetworkStatus.value = {
+    onlineStatus: "online",
+  }
+}
+
+const turnOfflineStatus = () => {
+  systemNetworkStatus.value = {
+    onlineStatus: "offline",
+  }
+}
+
 
 const selectBatteryIcon = (systemBatteryStatus: SystemBatteryStatus | null) => {
   if (systemBatteryStatus === null) {
@@ -33,6 +53,22 @@ const selectBatteryIcon = (systemBatteryStatus: SystemBatteryStatus | null) => {
   }
 }
 
+const selectNetworkIcon = (systemNetworkStatus: SystemNetworkStatus | null) => {
+  if (systemNetworkStatus === null) {
+    return "lucide:wifi-off";
+  }
+
+  if (systemNetworkStatus.onlineStatus === "offline") {
+    return "lucide:wifi-off";
+  }
+
+  if (systemNetworkStatus.onlineStatus === "online") {
+    return "lucide:wifi";
+  }
+
+  assertNever(systemNetworkStatus.onlineStatus);
+}
+
 onMounted(async () => {
   if ("getBattery" in navigator) {
     const battery = await navigator.getBattery();
@@ -46,16 +82,38 @@ onMounted(async () => {
       updateBatteryStatus(battery);
     });
   }
+
+  if (navigator.onLine) {
+    turnOnlineStatus();
+  } else {
+    turnOfflineStatus();
+  }
+
+  window.addEventListener("online", () => {
+    turnOnlineStatus();
+  })
+
+  window.addEventListener("offline", () => {
+    turnOfflineStatus();
+  })
+
+  hasMounted.value = true;
 })
 
 </script>
 
 <template>
-  <div class="system-status-line">
+  <div v-if="hasMounted" class="system-status-line">
     <div class="status-container">
       <Icon :icon="selectBatteryIcon(systemBatteryStatus)" class="icon" />
       <span v-if="systemBatteryStatus">{{ systemBatteryStatus.level }}%</span>
       <span v-else>BATTERY API UNAVAILABLE</span>
+    </div>
+    <div class="status-container">
+      <Icon :icon="selectNetworkIcon(systemNetworkStatus)" class="icon" />
+      <span v-if="!systemNetworkStatus">CONNECTION API UNAVAILABLE</span>
+      <span v-else-if="systemNetworkStatus.onlineStatus === 'online'">ONLINE</span>
+      <span v-else>OFFLINE</span>
     </div>
   </div>
 </template>
