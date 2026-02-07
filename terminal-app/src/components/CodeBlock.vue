10c6ce6c1eea12ca88f2e69277b8547e051455c0
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2026-02-07 16:14:06 +0200

    add codeblock copy functionality

--- terminal-app/src/components/CodeBlock.vue
+++ terminal-app/src/components/CodeBlock.vue
@@ -1,15 +1,40 @@
 <script setup lang="ts">
+import { Icon } from "@iconify/vue";
+import { ref } from "vue";
+
 interface Props {
   code: string;
 }
 
 const { code } = defineProps<Props>();
+const state = ref<"default" | "copied">("default");
+const selectIcon = () => {
+  if (state.value === "default") {
+    return "lucide:clipboard";
+  } else {
+    return "lucide:clipboard-check";
+  }
+}
+const handleCopy = async () => {
+  try {
+    await navigator.clipboard.writeText(code);
+    state.value = "copied";
+    setTimeout(() => {
+      state.value = "default";
+    }, 1250);
+  } catch (err) {
+    console.error("Failed to copy text: ", err);
+  }
+};
 </script>
 
 
 <template>
   <code>
-    {{ code }}
+    <span>
+      {{ code }}
+    </span>
+    <Icon :icon="selectIcon()" @click="handleCopy" class="icon" />
   </code>
 </template>
 
@@ -28,5 +53,15 @@ code {
   overflow-x: auto;
   line-height: 1.0;
   font-size: 1.25rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.icon {
+  color: color-mix(in srgb, var(--sky-300), transparent 25%);
+  width: 1.25rem;
+  height: 1.25rem;
+  cursor: pointer;
 }
 </style>
