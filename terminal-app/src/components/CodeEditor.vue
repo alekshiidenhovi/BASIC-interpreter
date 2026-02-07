0c1ac1aa05404303a2d0fc5bfd271d22b931ab7d
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2026-02-07 15:32:57 +0200

    make code editor lines editable

--- terminal-app/src/components/CodeEditor.vue
+++ terminal-app/src/components/CodeEditor.vue
@@ -10,12 +10,20 @@ type Statement = {
 const selectedMode = ref<Mode>('editor');
 const statements = ref<Statement[]>([{
   lineNumber: 10,
-  statement: ""
+  statement: "PRINT \"HELLO WORLD\""
 }, {
   lineNumber: 20,
   statement: "LET HELLO_VARIABLE = 10"
+}, {
+  lineNumber: 30,
+  statement: "PRINT HELLO_VARIABLE"
+},
+{
+  lineNumber: 40,
+  statement: "END"
 }]);
-const printLines = ref<string[]>(["my variable value: hello basic", "my expression value: 10"]);
+const printLines = ref<string[]>([]);
+
 </script>
 
 <template>
@@ -27,13 +35,14 @@ const printLines = ref<string[]>(["my variable value: hello basic", "my expressi
         @click="selectedMode = 'repl'">REPL</button>
     </div>
     <div class="editor-content">
-      <div class="editor-statement" v-for="statement in statements" :key="statement.lineNumber">
-        <span>{{ statement.lineNumber }}</span>
-        <span>{{ statement.statement }}</span>
+      <div class="editor-statement" v-for="(statement, index) in statements" :key="index">
+        <input v-model.number="statement.lineNumber" class="line-number-input" type="number" />
+        <input v-model="statement.statement" class="statement-input"
+          @keydown.enter="statements.push({ lineNumber: (statements[statements.length - 1]?.lineNumber || 0) + 10, statement: '' })" />
       </div>
     </div>
     <div class="editor-print-container">
-      <p class="editor-print-line" v-for="line in printLines" :key="line">{{ line }}</p>
+      <p class="editor-print-line" v-for="(line, index) in printLines" :key="index">{{ line }}</p>
     </div>
   </div>
 </template>
@@ -69,13 +78,13 @@ const printLines = ref<string[]>(["my variable value: hello basic", "my expressi
   letter-spacing: 3px;
 }
 
+.editor-mode-button:nth-child(1) {
+  border-right: 2px solid var(--sky-900);
+}
+
 .selected-mode {
   color: var(--sky-500);
   font-weight: 700;
-}
-
-.editor-mode-button:nth-child(1) {
-  border-right: 2px solid var(--sky-900) !important;
 }
 
 .editor-content,
@@ -84,11 +93,38 @@ const printLines = ref<string[]>(["my variable value: hello basic", "my expressi
 }
 
 .editor-statement {
-  color: var(--sky-500);
   font-size: 1.25rem;
   display: flex;
   gap: 1rem;
   padding: 0.25rem 0.75rem;
+}
+
+.line-number-input {
+  background: transparent;
+  border: none;
+  color: var(--sky-600);
+  width: 3rem;
+  font-family: inherit;
+  font-size: inherit;
+  outline: none;
+  -moz-appearance: textfield;
+}
+
+.line-number-input::-webkit-outer-spin-button,
+.line-number-input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+.statement-input {
+  background: transparent;
+  border: none;
+  color: var(--sky-400);
+  flex: 1;
+  font-family: inherit;
+  font-size: inherit;
+  outline: none;
+  text-transform: uppercase;
 }
 
 .editor-print-line {
