d7df0adb7de01d23e8249f94f53f903eb138d0c8
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2026-02-13 20:10:51 +0200

    fix js casts

--- interpreter/web/main.dart
+++ interpreter/web/main.dart
@@ -19,28 +19,29 @@ void main() {
             try {
               final lexer = Lexer(code.toDart);
               final tokens = lexer.tokenize();
+
               if (outputMode.toDart == "lexer") {
-                return tokens.map((t) => t.toString()).toList();
+                return tokens.map((t) => t.toString().toJS).toList().toJS;
               }
 
               final parser = Parser(tokens);
               final programLines = parser.parse();
+
               if (outputMode.toDart == "parser") {
                 return programLines.values
                     .map((p) => p.toString().toJS)
-                    .toList();
+                    .toList()
+                    .toJS;
               }
 
               final interpreter = Interpreter(programLines);
-              final result = interpreter.interpret();
+              final List<String> result = interpreter.interpret();
 
               return result.map((s) => s.toJS).toList().toJS;
             } on BASICInterpreterError catch (e) {
-              return [
-                "${e.interpreterStepErrorName}: ${e.toString()}".toJS,
-              ].toList().toJS;
+              return ["${e.interpreterStepErrorName}: $e".toJS].toJS;
             } catch (e) {
-              return ["Unknown error: $e".toJS].toList().toJS;
+              return ["Unknown error: $e".toJS].toJS;
             }
           }).toJS
           as JSFunction;
