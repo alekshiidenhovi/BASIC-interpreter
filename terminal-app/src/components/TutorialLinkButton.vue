7beb60b35e82ccecd476c2a4d463caa31cf60a8f
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2026-02-18 23:09:21 +0200

    add TutorialLinkButton component

---
+++ terminal-app/src/components/TutorialLinkButton.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface Props {
+  label: string;
+  href: string;
+}
+const { label, href } = defineProps<Props>();
+</script>
+
+<template>
+  <a :href="href">
+    {{ label }}
+  </a>
+</template>
+
+<style scoped>
+a {
+  color: var(--sky-400);
+  background-color: color-mix(in srgb, var(--slate-900) 50%, transparent);
+  border-radius: 2px;
+  padding: 1.25rem 2rem;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-size: 1.25rem;
+  border: 2px solid var(--sky-400);
+}
+</style>
